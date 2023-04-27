eb5535453462b284d85509609981a8d2174ec5c3
SugitaMotoki <ce235019@meiji.ac.jp>
2023-04-28 02:13:49 +0900

    firestoreの最低限の実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -41,12 +42,31 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Firebase Demo"),
+      ),
       body: Center(
         child: Container(
           padding: const EdgeInsets.all(24),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              TextButton(
+                onPressed: () {
+                  FirebaseFirestore.instance
+                      .collection("chiken")
+                      .doc("uoz3s5WsKXDi9c29X7r2")
+                      .get()
+                      .then((ref) {
+                    print(ref.data());
+                  });
+                  print("button pressed");
+                },
+                child: const Text(
+                  "execute",
+                  style: TextStyle(fontSize: 50),
+                ),
+              ),
               TextFormField(
                 decoration: const InputDecoration(labelText: 'Email'),
                 onChanged: (String value) {
